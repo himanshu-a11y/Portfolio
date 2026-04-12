15304f9553c943360533a6bbb10119efaa9f7620
Himanshu Gurjar <anshudmiz2002@gmail.com>
2026-04-12 14:47:39 +0530

    depoly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://portfolio-s8ik.onrender.com',
         changeOrigin: true,
       }
     }
